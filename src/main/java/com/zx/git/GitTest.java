358d0baf7a06366515eec743cc625443bba49a45
zx <zx@123.com>
2023-05-27 20:26:43 +0800

    third commit

--- src/main/java/com/zx/git/GitTest.java
+++ src/main/java/com/zx/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello the world");
         System.out.println("hello the world1");
+        System.out.println("hello the w");
     }
 
 }
