fe66ddd0bc6d92c1122e297055a736bf7e74f1c2
zx <zx@123.com>
2023-05-28 10:27:04 +0800

    four commit

--- src/main/java/com/zx/git/GitTest.java
+++ src/main/java/com/zx/git/GitTest.java
@@ -1,7 +1,7 @@
 package com.zx.git;
 
 public class GitTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) {d
         System.out.println("hello the world");
         System.out.println("hello the world1");
         System.out.println("hello the w");
